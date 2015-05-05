3ab1d20f3258464fc36431877aea132ad89d33b5
baidut <13591120447@163.com>
2015-05-05 16:09:42 +0800

    代码微调

--- stud_info_manager/main.c
+++ stud_info_manager/main.c
@@ -1,3 +1,9 @@
+/*
+ * Student Information Manager
+ * https://baidut.github.io/ | Released under MIT license
+ * Copyright (c) 2015 Zhenqiang.Ying <yingzhenqiang@gmail.com>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,39 +11,36 @@
 #define MAX      100
 #define NAME_LEN 10
 
-typedef struct{
-	char name[NAME_LEN];
-	unsigned int score[3];
-	unsigned int sum;
-}Stu_t;
+typedef struct {
+    char name[NAME_LEN];
+    unsigned int score[3];
+    unsigned int sum;
+} Stu_t;
 
-Stu_t 	stu[100];
+Stu_t   stu[MAX];
 int     num;
 
-Stu_t 	input_stu(void);    // 返回输入的学生信息
-void 	output_stu(int i);  // 根据数组索引显示对应的学生信息
-int 	find_stu(void);     // 查找并显示数据，返回数组索引
+Stu_t   input_stu(void);                    // 通过提示引导用户正确地输入一条学生记录，并返回这条记录
+void    output_stu(int i);                  // 根据数组索引打印出对应的学生信息
+int     find_stu(void);                     // 通过提示引导用户查找记录，如果找到则显示查找到的记录，并返回数组索引，否则返回-1
 
-void    read_stu(const char* filename);
-void    write_stu(const char* filename);
+void    read_stu(const char *filename);     // 根据文件名读入文件中的学生数据
+void    write_stu(const char *filename);    // 将学生数据写入到指定的文件中
 
-int     confirm(const char* words);
-
-void safe_flush(FILE *fp) {
-    int ch;
-    while( (ch = fgetc(fp)) != EOF && ch != '\n' );
-}
+int     confirm(const char *words);         // 请求用户确认，输入y或Y返回1，输入n或N返回0，其他情况提示用户重新输入
+void    safe_flush(FILE *fp);               // safe_flush(stdin)清理多余的输入，防止影响下次获取输入
 
 int main(void) {
-    const char* filename = "stud_info.txt";
-    read_stu(filename);// 读取数据
+    const char *filename = "stud_info.txt";
+
+    read_stu(filename); // 导入数据
     printf("欢迎使用学生成绩管理系统\n"
-           "-当前已有%d条记录,本程序限制%d条记录\n"
-           ,num,MAX);
-    // 主菜单
-    for(;;){
+           "-当前已有%d条记录,本程序限制%d条记录\n",
+           num, MAX);
+
+    for (;;) {
     char choice;
-    int i,j;
+    int i, j;
 
         printf( "-菜单:\t"
                 "1.查找\t"
@@ -53,149 +56,155 @@ int main(void) {
         choice = getchar();
         safe_flush(stdin);
 
-        switch(choice){
-            case '1': // 查找
-                find_stu();
-                break;
-            case '2': // 追加
-                if(num<MAX){
-                    stu[num] = input_stu();
-                    output_stu(num);
-                    num++;
-                    puts("追加成功!");
-                }
-                else puts("记录已满，无法追加！");
-                break;
-            case '3': // 删除
-                i = find_stu();
-                if(-1!=i){
-                    if( confirm("确认删除？") ){
-                        while(i++ != num){
-                            stu[i-1] = stu[i];
-                        }
-                        num--;
-                        puts("删除成功!");
+        switch (choice) {
+        case '1': // 查找
+            find_stu();
+            break;
+        case '2': // 追加
+            if (num < MAX) {
+                stu[num] = input_stu();
+                output_stu(num);
+                num++;
+                puts("追加成功!");
+            }
+            else puts("记录已满，无法追加！");
+            break;
+        case '3': // 删除
+            i = find_stu();
+            if (-1 != i) {
+                if (confirm("确认删除？")) {
+                    while (i++ != num) {
+                        stu[i-1] = stu[i];
                     }
+                    num--;
+                    puts("删除成功!");
                 }
-                break;
-            case '4': // 修改
-                i = find_stu();
-                if(-1!=i){
-                    stu[i] = input_stu();
-                    output_stu(i);
-                    puts("修改成功！");
-                }
-                break;
-            case '5': // 按照总分排序
-                for(i = 0;i < num-1;i++) // 冒泡排序已经排好的
-                    for(j = 0;j < num-1-i;j++) // 遍历没排好的
-                        if( stu[j].sum < stu[j+1].sum ) {
-                            Stu_t t = stu[j];
-                            stu[j] = stu[j+1];
-                            stu[j+1] = t;
-                        }
-                puts("排序成功！");
-                /* fall through */     // 完成排序后自动显示
-            case '6': // 显示
-                for(i = 0;i < num;i++){
-                    printf("%4d:\t",i+1);
-                    output_stu(i);
-                }
-                break;
-            case '7':
+            }
+            break;
+        case '4': // 修改
+            i = find_stu();
+            if (-1 != i) {
+                stu[i] = input_stu();
+                output_stu(i);
+                puts("修改成功！");
+            }
+            break;
+        case '5': // 按照总分排序
+            for (i = 0; i < num-1; i++)     // 冒泡排序中已经排好的
+                for (j=0; j < num-1-i; j++) // 遍历没排好的
+                    if ( stu[j].sum < stu[j+1].sum ) {
+                        Stu_t t = stu[j];
+                        stu[j] = stu[j+1];
+                        stu[j+1] = t;
+                    }
+            puts("排序成功！");
+            /* fall through */     // 完成排序后自动显示
+        case '6': // 显示
+            for (i = 0; i < num; i++) {
+                printf("%4d:\t", i+1);
+                output_stu(i);
+            }
+            break;
+        case '7': // 保存
+            write_stu(filename);
+            break;
+        case '0': // 退出
+            if (confirm("推出前保存？（Y/N）")) {
                 write_stu(filename);
-                break;
-            case '0':
-                if( confirm("推出前保存？（Y/N）") ){
-                    write_stu(filename);
-                }
-                exit(0);
-            default:
-                puts("没有该序号！请重新输入");
-                break;
+            }
+            exit(0);
+        default:
+            puts("没有该序号！请重新输入");
+            break;
         }
-    } // END FOR
+    } // END for
 
-	return 0;
+    return 0;
 }
 
-int confirm(const char* words){
+void safe_flush(FILE *fp){
+    int ch;
+    while ( ch = fgetc(fp), ch != EOF && ch != '\n');
+}
+
+int confirm(const char *words) {
     char choice;
     puts(words);
-    for(;;){
+    for (;;) {
         choice = getchar();
         safe_flush(stdin);
 
-        switch(choice){
-            default:
-                puts("输入非法，重新输入！");
-                continue;
-            case 'Y':
-            case 'y':
-                puts("已确认！");
-                return 1;
-            case 'N':
-            case 'n':
-                puts("已取消！");
-                return 0;
+        switch (choice) {
+        default:
+            puts("输入非法，重新输入！");
+            continue;
+        case 'Y':
+        case 'y':
+            puts("已确认！");
+            return 1;
+        case 'N':
+        case 'n':
+            puts("已取消！");
+            return 0;
         }
     }
 }
 
 Stu_t input_stu(void) {
-	Stu_t ret;
-	puts("依次输入姓名、语文、数学、外语成绩，空格分隔，成绩要求是整数:");
-	while( 4 != scanf("%s%d%d%d",ret.name,
-								 &ret.score[0],
-			                     &ret.score[1],
-			                     &ret.score[2] )){
+    Stu_t ret;
+    puts("依次输入姓名、语文、数学、外语成绩，空格分隔，成绩要求是整数:");
+    while ( 4 != scanf("%s%d%d%d", ret.name,
+                                  &ret.score[0],
+                                  &ret.score[1],
+                                  &ret.score[2])) {
         safe_flush(stdin);
         puts("输入有误，请重新输入!");
     }
-    safe_flush(stdin);// 否则回车会污染
+    safe_flush(stdin);
     ret.sum = ret.score[0] + ret.score[1] + ret.score[2];
-	return ret;
+    return ret;
 }
 
 void output_stu(int i) {
-	if(i < 0 || i > MAX) return;
-	printf("%s\t语:%d\t数:%d\t外:%d\t总分：%d\n",
-								stu[i].name,
-								stu[i].score[0],
-								stu[i].score[1],
-								stu[i].score[2],
-								stu[i].sum);
+    if (i < 0 || i > MAX) return;
+    printf("%s\t语:%d\t数:%d\t外:%d\t总分：%d\n",
+                                stu[i].name,
+                                stu[i].score[0],
+                                stu[i].score[1],
+                                stu[i].score[2],
+                                stu[i].sum);
 }
 
 int find_stu(void) {
     char name[NAME_LEN];
     int i;
-	printf("输入姓名（限%d个字符）：",NAME_LEN);
-	scanf("%s",name);
-	safe_flush(stdin);
-	for(i = 0;i < num;i++){
-		if(0==strcmp(stu[i].name,name)){
-			output_stu(i);
-			return i;
-		}
-	}
-	puts("没有找到记录!");
-	return -1;
+    printf("输入姓名（限%d个字符）：", NAME_LEN);
+    scanf("%s", name);
+    safe_flush(stdin);
+    for (i = 0; i < num; i++) {
+        if (0 == strcmp(stu[i].name, name)) {
+            output_stu(i);
+            return i;
+        }
+    }
+    puts("没有找到记录!");
+    return -1;
 }
-void read_stu(const char* filename){
-    FILE* fp = fopen(filename,"rt");
-    if(NULL != fp) {
-        fread( &num,sizeof(int),1,fp);
-        fread( stu,sizeof(Stu_t),num,fp);
+
+void read_stu(const char *filename) {
+    FILE *fp = fopen(filename, "rt");
+    if (NULL != fp) {
+        fread(&num, sizeof(int), 1, fp);
+        fread(stu, sizeof(Stu_t), num, fp);
     }
     fclose(fp);
 }
 
 void write_stu(const char* filename) {
-    FILE* fp = fopen(filename,"wt");
-    if(NULL != fp) {
-        fwrite( &num,sizeof(int),1,fp);
-        fwrite( stu,sizeof(Stu_t),num,fp);
+    FILE *fp = fopen(filename, "wt");
+    if (NULL != fp) {
+        fwrite(&num, sizeof(int), 1, fp);
+        fwrite(stu, sizeof(Stu_t), num, fp);
         fclose(fp);
         puts("保存成功!");
     }
